ab541aa3f8ea7c16230041f7f883f90104488254
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2018-07-10 17:05:57 -0400

    Add additional test to ensure schema is being placed in correct location

--- ktor-swagger/src/test/kotlin/de/nielsfalk/ktor/swagger/SwaggerSupportTest.kt
+++ ktor-swagger/src/test/kotlin/de/nielsfalk/ktor/swagger/SwaggerSupportTest.kt
@@ -6,6 +6,9 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.gson.GsonConverter
 import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
+import io.ktor.locations.Location
+import io.ktor.locations.Locations
+import io.ktor.routing.routing
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
 import org.junit.Test
@@ -17,9 +20,18 @@ class SwaggerSupportTest {
         application.install(SwaggerSupport) { forwardRoot = true }
 
         // then
-        handleRequest(HttpMethod.Get, "/").response.headers["Location"].should.equal("/apidocs/index.html?url=swagger.json")
-        handleRequest(HttpMethod.Get, "/apidocs").response.headers["Location"].should.equal("/apidocs/index.html?url=swagger.json")
-        handleRequest(HttpMethod.Get, "/apidocs/").response.headers["Location"].should.equal("/apidocs/index.html?url=swagger.json")
+        handleRequest(
+            HttpMethod.Get,
+            "/"
+        ).response.headers["Location"].should.equal("/apidocs/index.html?url=swagger.json")
+        handleRequest(
+            HttpMethod.Get,
+            "/apidocs"
+        ).response.headers["Location"].should.equal("/apidocs/index.html?url=swagger.json")
+        handleRequest(
+            HttpMethod.Get,
+            "/apidocs/"
+        ).response.headers["Location"].should.equal("/apidocs/index.html?url=swagger.json")
     }
 
     @Test
@@ -28,7 +40,10 @@ class SwaggerSupportTest {
         application.install(SwaggerSupport) { forwardRoot = true }
 
         // then
-        handleRequest(HttpMethod.Get, "/apidocs/index.html").response.content.should.contain("<title>Swagger UI</title>")
+        handleRequest(
+            HttpMethod.Get,
+            "/apidocs/index.html"
+        ).response.content.should.contain("<title>Swagger UI</title>")
     }
 
     @Test
@@ -45,5 +60,33 @@ class SwaggerSupportTest {
             method = HttpMethod.Get
             addHeader("Accept", ContentType.Application.Json.toString())
         }.response.content.should.contain("\"swagger\":\"2.0\"")
+    }
+
+    @Location("/model")
+    private class modelRoute
+
+    private class Model(val value: String)
+
+    @Test
+    fun `provide swaggerJson when a custom schema is provided`(): Unit = withTestApplication {
+        // when
+        application.install(ContentNegotiation) {
+            register(ContentType.Application.Json, GsonConverter())
+        }
+        application.install(SwaggerSupport) {
+            forwardRoot = true
+        }
+        application.install(Locations)
+
+        application.routing {
+            put<modelRoute, Model>(body(mapOf("type" to "object"))) { _, _ -> }
+        }
+
+        // then
+        handleRequest {
+            uri = "/apidocs/swagger.json"
+            method = HttpMethod.Get
+            addHeader("Accept", ContentType.Application.Json.toString())
+        }.response.content.should.contain("\"swagger\":\"2.0\"").and.contain("\"type\":\"object\"")
     }
 }
