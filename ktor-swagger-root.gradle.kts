0ddfc04bff5b9067f56bdd2624c922fa914969e5
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2018-07-20 16:54:17 -0400

    Release version 0.2.2

--- ktor-swagger-root.gradle.kts
+++ ktor-swagger-root.gradle.kts
@@ -32,7 +32,7 @@ allprojects {
         plugin("com.diffplug.gradle.spotless")
     }
     group = "de.nielsfalk.ktor"
-    version = "0.2.1"
+    version = "0.2.2"
 
     repositories {
         mavenCentral()
