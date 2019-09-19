a07e8dc59e455d81f1444f41e2588e7c6ce54b38
Alena Egorova <catincosmicspace@gmail.com>
2019-09-19 13:32:42 +0300

    new version of swagger ui

--- ktor-swagger/src/main/kotlin/de/nielsfalk/ktor/swagger/SwaggerUi.kt
+++ ktor-swagger/src/main/kotlin/de/nielsfalk/ktor/swagger/SwaggerUi.kt
@@ -19,7 +19,7 @@ class SwaggerUi {
             in notFound -> return
             null -> return
             else -> {
-                val resource = this::class.java.getResource("/META-INF/resources/webjars/swagger-ui/3.17.4/$filename")
+                val resource = this::class.java.getResource("/META-INF/resources/webjars/swagger-ui/3.23.8/$filename")
                 if (resource == null) {
                     notFound.add(filename)
                     return
