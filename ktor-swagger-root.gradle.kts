04c93853406821b0e0fde0e3c6533192b2fc8b48
😈 <dr.franken@protonmail.ch>
2019-12-16 09:46:22 +0100

    Bumped Version to 0.6.0

--- ktor-swagger-root.gradle.kts
+++ ktor-swagger-root.gradle.kts
@@ -32,7 +32,7 @@ allprojects {
         plugin("com.diffplug.gradle.spotless")
     }
     group = "de.nielsfalk.ktor"
-    version = "0.5.0"
+    version = "0.6.0"
 
     repositories {
         mavenCentral()
