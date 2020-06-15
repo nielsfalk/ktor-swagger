21fe6939e10c5ff6023d84f8943743e46c8dfac9
Jonathan Leitschuh <jonathan.leitschuh@gmail.com>
2020-06-15 15:46:40 -0400

    Release 0.7.0

--- ktor-swagger-root.gradle.kts
+++ ktor-swagger-root.gradle.kts
@@ -32,7 +32,7 @@ allprojects {
         plugin("com.diffplug.gradle.spotless")
     }
     group = "de.nielsfalk.ktor"
-    version = "0.6.0"
+    version = "0.7.0"
 
     repositories {
         mavenCentral()
